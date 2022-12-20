0f23fb8bc135bbad82af7dec361c807fd857766b
Zuhayr Syed <zuhxyr.s6@hotmail.com>
2022-12-20 00:13:43 -0500

    Create object with name, price, and link

--- web_scrape.py
+++ web_scrape.py
@@ -24,14 +24,25 @@ else:
     page_text = doc.find(class_='list-tool-pagination-text').strong.text
     pages = int(page_text[2])
     for page in range(1, pages + 1):
-        print(page)
         doc = extract(page)
         div = doc.find(class_='item-cells-wrap border-cells items-grid-view four-cells expulsion-one-cell')
         
         # find items that contain the search in name using regular expression
         items = div.find_all(text=re.compile(product))
 
+        itemsList = {}
+
         for item in items:
-            print(item)
+            parent = item.parent
+            if parent.name != 'a':
+                # for items that are not a listing and dont have a link
+                continue
+            link = parent['href']
+            
+            main_parent = item.find_parent(class_='item-container')
+            price = main_parent.find(class_='price-current').strong.string
+            
+            itemsList[item] = {"price": price, "link": link}
+print(itemsList)
 
     
