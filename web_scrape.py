22c855657cc98cf4c69aac0d9cdb62b323b5af06
Zuhayr Syed <zuhxyr.s6@hotmail.com>
2022-12-20 00:28:04 -0500

    Fix format and make print statement more clear

--- web_scrape.py
+++ web_scrape.py
@@ -2,13 +2,18 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
+printLine = '-------------------------------------------------------------------------------------------------------'
+
+# makes get request to page 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'}
-
-
 def extract(page):
     link = f'https://www.newegg.ca/p/pl?d={product}&N=4131&page={page}'
     page = requests.get(link, headers).text
     return BeautifulSoup(page, 'html.parser')
+
+"""
+Scrapes data from Newegg for a searched product 
+"""
 
 product = input("What product do you want to search for? ")
 
@@ -18,8 +23,10 @@ doc = extract(1)
 # not found error check
 error = doc.find(class_='result-message-error')
 if error:
-    print('ERROR WITH SEARCH: ' + error.text)
+    print('\n' + printLine + '\n' + 'ERROR WITH SEARCH: ' + error.text + '\n' + printLine + '\n')
 else:
+    print('\n' + printLine + '\n' + 'LOADING RESULTS...' + '\n' + printLine + '\n')
+    
     # find number of pages
     page_text = doc.find(class_='list-tool-pagination-text').strong.text
     pages = int(page_text[2])
@@ -43,6 +50,6 @@ else:
             price = main_parent.find(class_='price-current').strong.string
             
             itemsList[item] = {"price": price, "link": link}
-print(itemsList)
+    print(itemsList)
 
     
